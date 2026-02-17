913d9e1989bff52f2db3a5924c5d5ee315f3dd7e
Davi de Sousa <davisouza2204@gmail.com>
2026-02-17 14:44:31 -0300

    first commig

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Iniciando...");
+    }
+}
